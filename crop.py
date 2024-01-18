ce5049ec31fcf3131ad7c0835e850dd2ad3e76c4
losjashik <144885616+losjashik@users.noreply.github.com>
2024-01-18 14:41:43 +0100

    Update crop.py
    added doc strings

--- crop.py
+++ crop.py
@@ -2,6 +2,17 @@ import cv2
 import os
 
 def crop_and_save_images(input_folder, output_folder, target_size):
+     """
+    Crop images from the input folder to a square of the specified target size and save them in the output folder.
+
+    Parameters:
+    - input_folder (str): The path to the folder containing input images.
+    - output_folder (str): The path to the folder where cropped images will be saved.
+    - target_size (int): The size (in pixels) of the square to which the images will be cropped.
+
+    Example:
+    >>> crop_and_save_images("input_photos", "output_photos", 700)
+    """
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
